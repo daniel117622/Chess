658c59fe185721f8244fbbb9340cbf8b279df79f
daniel117622 <dan5jul@hotmail.com>
2023-07-05 02:59:04 -0600

    python returns random move

--- server/script.py
+++ server/script.py
@@ -1,15 +1,20 @@
 import sys
 import json
-
+import chess
+import random
 # Read the JSON input from the command line arguments
 json_input = json.loads(sys.argv[1])
-
+board = chess.Board(json_input["fenString"])
+move=random.choice(list(board.legal_moves))
 # Perform some processing on the JSON data (e.g., modify it)
 # Here, we simply add a new key-value pair to the JSON object
 json_input['processed'] = True
+res = {"move":move}
 
-# Convert the modified JSON back to a string
-json_output = json.dumps(json_input)
+# Apply the move to the board
+board.push(move)
 
+# Get the FEN string after applying the move
+fen_string = board.fen()
 # Print the modified JSON to be captured by the Node.js script
-print(json_output)
+print(json.dumps({"move":str(fen_string)}))
